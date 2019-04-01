66b5a051c12628713b1460ffa9b5e73456c8bd1f
icegarlic <577530562@qq.com>
2019-04-01 20:04:46 +0800

    second

--- src/game/Game2048.java
+++ src/game/Game2048.java
@@ -56,8 +56,8 @@ public class Game2048 extends JPanel {
             @Override
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
-                    case KeyEvent.VK_UP;
-                        moveUP();
+                    case KeyEvent.VK_UP:
+                        moveUp();
                         break;
                     case KeyEvent.VK_DOWN:
                         moveDown();
@@ -95,6 +95,157 @@ public class Game2048 extends JPanel {
 
     void drawGird(Graphics2D graphics2D) {
         graphics2D.setColor(gridColor);
+        graphics2D.fillRoundRect(200, 100, 499, 499, 15, 15);
+
+        if (gamestate == State.running) {
+            for (int r = 0; r < side; r++) {
+                for (int c = 0; c < side; c++) {
+                    if (tiles[r][c] == null) {
+                        graphics2D.setColor(emptyColor);
+                        graphics2D.fillRoundRect(215 + c * 121, 155 + r * 121, 106, 106, 7, 7);
+                    } else {
+                        drawTile(graphics2D, r, c);
+                    }
+                }
+            }
+        } else {
+            graphics2D.setColor(startColor);
+            graphics2D.fillRoundRect(215, 115, 469, 469, 7, 7);
+
+            graphics2D.setColor(gridColor.darker());
+            graphics2D.setFont(new Font("HUAWEI", Font.BOLD, 128));
+            graphics2D.drawString("2048", 310, 270);
+
+            graphics2D.setFont(new Font("HUAWEI", Font.BOLD, 20));
+
+            if (gamestate == State.won) {
+                graphics2D.drawString("you made it", 390, 350);
+            } else if (gamestate == State.over) {
+                graphics2D.drawString("game over", 400, 350);
+            }
+
+            graphics2D.setColor(gridColor);
+            graphics2D.drawString("click to start a new game", 330, 470);
+            graphics2D.drawString("use arrow keys to move tiles", 310, 530);
+        }
+    }
+
+    void drawTile(Graphics2D graphics2D, int r, int c) {
+        int value = tiles[r][c].getValue();
+
+        graphics2D.setColor(colorTable[(int) (Math.log(value) / Math.log(2)) + 1]);
+        graphics2D.fillRoundRect(215 + c * 121,115 + r * 121,106,106,7,7);
+        String s = String.valueOf(value);
+
+        graphics2D.setColor(value < 128 ? colorTable[0] : colorTable[1]);
+
+        FontMetrics fontMetrics = graphics2D.getFontMetrics();
+        int asc = fontMetrics.getAscent();
+        int dec = fontMetrics.getDescent();
+
+        int x = 215 + c * 121 + (106 - fontMetrics.stringWidth(s)) / 2;
+        int y = 115 + r * 121 + (asc + (106 - (asc + dec)) / 2);
+        graphics2D.drawString(s, x, y);
+    }
+
+    private void addRandomTile() {
+        int pos = random.nextInt(side * side);
+        int row, col;
+        do {
+            pos = (pos + 1) % (side * side);
+            row = pos / side;
+            col = pos % side;
+        } while (tiles[row][col] != null);
+
+        int val = random.nextInt(10) == 0 ? 4 : 2;
+        tiles[row][col] = new Tile(val);
+    }
+
+    private boolean move(int countDownFrom, int yIncr, int xIncr) {
+        boolean moved = false;
+
+        for (int i = 0; i < side * side; i++) {
+            int j = Math.abs(countDownFrom - i);
+
+            int r = j / side;
+            int c = j % side;
+
+            if (tiles[r][c] == null)
+                continue;
+            int nextR = r + yIncr;
+            int nextC = c + xIncr;
+
+            while (nextR >= 0 && nextR < side && nextC >= 0 && nextC < side) {
+                Tile next = tiles[nextR][nextC];
+                Tile curr = tiles[r][c];
+
+                if (next == null) {
+                    if (checkingAvailableMoves)
+                        return true;
+                    tiles[nextR][nextC] = curr;
+                    tiles[r][c] = null;
+                    r = nextR;
+                    c = nextC;
+                    nextR += yIncr;
+                    nextC += xIncr;
+                    moved = true;
+                } else if (next.canMergeWith(curr)) {
+
+                    if (checkingAvailableMoves)
+                        return true;
+
+                    int value = next.mergeWith(curr);
+                    if (value > highest)
+                        highest = value;
+                    score += value;
+                    tiles[r][c] = null;
+                    moved = true;
+                    break;
+                } else
+                    break;
+            }
+        }
+        if (moved) {
+            if (highest < target) {
+                clearMerged();
+                addRandomTile();
+                if (!movesAvailable()) {
+                    gamestate = State.over;
+                }
+            } else if (highest == target)
+                gamestate = State.won;
+        }
+        return moved;
+    }
+
+    boolean moveUp() {
+        return move(0, -1, 0);
+    }
+
+    boolean moveDown() {
+        return move(side * side - 1, 1, 0);
+    }
+
+    boolean moveLeft() {
+        return move(0, 0, -1);
+    }
+
+    boolean moveRight() {
+        return move(side * side - 1, 0, 1);
+    }
+
+    void clearMerged() {
+        for (Tile[] row : tiles)
+            for (Tile tile : row)
+                if (tile != null)
+                    tile.setMerged(false);
+    }
+
+    boolean movesAvailable() {
+        checkingAvailableMoves = true;
+        boolean hasMoves = moveUp() || moveDown() || moveLeft() || moveRight();
+        checkingAvailableMoves = false;
+        return hasMoves;
     }
 
 }
---
+++ src/game/Main.java
@@ -0,0 +1,19 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame();
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setTitle("2048");
+            f.setResizable(true);
+            f.add(new Game2048(), BorderLayout.CENTER);
+            f.pack();
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+        });
+    }
+}
